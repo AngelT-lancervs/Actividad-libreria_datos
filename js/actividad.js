17c02e86205ea36923cc6b335a0d973513efff69
Angel Tomalá <108084684+AngelT-lancervs@users.noreply.github.com>
2023-11-10 00:35:57 +0000

    avance en la actividad

--- js/actividad.js
+++ js/actividad.js
@@ -1,84 +1,96 @@
-let plantilla = `
-<div class="col-lg-2 mb-2 text-center">
-  <div class="card border-0 rounded-0">
-    <div class="card-image">
-      <img src="key:Image-URL-M" alt="blog-img" class="img-fluid">
-    </div>
-  </div>
-  <div class="card-body text-capitalize">
-    <div class="card-meta fs-6">
-      <span class="meta-date"> <!--tag:Book-Author--> </span>
-      <span class="meta-category">/ <a href="blog.html"> <!--tag:Year-Of-Publication--> </a></span>
-    </div>
-    <h4 class="card-title">
-      <a href="buy.html"> <!--tag:Book-Title--> </a>
-    </h4>
-  </div>
-</div>`
 
-let cargarLibrary = () => {
+
+let loadJson = async () => {
 
     let URL_JSON = 'https://raw.githubusercontent.com/DAWMFIEC/DAWM-apps/datos/bookstore-images.json';
-    let URL_XML = 'https://raw.githubusercontent.com/DAWMFIEC/DAWM-apps/datos/bookstore-books.xml';
 
-    fetch(URL_JSON)
-        .then(responseText => responseText.json())
-        .then(responseJSON => {
+    let responseObject = await fetch(URL_JSON);
+    let responseJSON = await responseObject.json();
 
-            console.log(responseJSON);
-
-
-        })
+    console.log(responseJSON);
+    return responseJSON;    
 
 }
 
-let parseXML = (responseText) => {
 
-    // Parsing XML
-    const parser = new DOMParser();
-    const xml = parser.parseFromString(responseText, "application/xml");
+//Cargar archivo XML
+let parseXML = async (responseText) => {
+
+  // Parsing XML
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(responseText, "application/xml");
+
+  console.log(xml)
+
+  let booksArr = xml.querySelectorAll("book");
 
 
-    // Referencia al elemento `#forecastbody` del documento HTML
+  return booksArr;
+}
 
-    let forecastElement = document.querySelector("#posts")
-    forecastElement.innerHTML = ''
+//carga imagenes por el isbn
+let loadImage = async (isbn) => {
 
-    // Procesamiento de los elementos con etiqueta `<book>` del objeto xml
-    let booksArr = xml.querySelectorAll("book")
+  let imagesArray = await loadJson(); // arreglo de imagenes 
 
-    booksArr.forEach(book => {
+  let imageUrl = "";  // Variable para almacenar la URL de la imagen
 
-        let from = book.getAttribute("from").replace("T", " ");
+  imagesArray.forEach((book) => {
+    let isbn_actual = book.querySelector("ISBN").textContent;
 
-        let book_author = book.querySelector("Book-Author");
-        let year_book = book.querySelector("Year-Of-Publication");
-        let book_title = book.querySelector("Book-Title");
+    if (isbn_actual === isbn) {
+      imageUrl = book.querySelector("Image-URL-M").textContent;
+    }
+  });
 
-        let template = `
-        <div class="col-lg-2 mb-2 text-center">
-          <div class="card border-0 rounded-0">
-            <div class="card-image">
-              <img src="key:Image-URL-M" alt="blog-img" class="img-fluid">
-            </div>
-          </div>
-          <div class="card-body text-capitalize">
-            <div class="card-meta fs-6">
-              <span class="meta-date"> <!--tag:Book-Author--> </span>
-              <span class="meta-category">/ <a href="blog.html"> <!--tag:Year-Of-Publication--> </a></span>
-            </div>
-            <h4 class="card-title">
-              <a href="buy.html"> <!--tag:Book-Title--> </a>
-            </h4>
-          </div>
-        </div>`
-        
+  return imageUrl;
+}
 
-        //Renderizando la plantilla en el elemento HTML
-        forecastElement.innerHTML += template;
-    })
+//combina info con imagenes de los libros
+let loadBooks = async () => {
+
+  let URL_XML = 'https://raw.githubusercontent.com/DAWMFIEC/DAWM-apps/datos/bookstore-books.xml';
+
+  let responseObject = await fetch(URL_XML);
+  let responseText = await responseObject.text();
+
+  let infoArray = await parseXML(responseText); //arreglo de libros con info
+
+  //Cargo la información del XML
+  let postsElement = document.querySelector("#library");
+  postsElement.innerHTML = '';
+
+  infoArray.forEach( (book) => {
+
+    let book_author = book.querySelector("Book-Author").textContent;
+    let year_publication = book.querySelector("Year-Of-Publication").textContent;
+    let book_title = book.querySelector("Book-Title").textContent;
+    let url_image = loadImage(book.querySelector("ISBN").textContent);
+
+
+    let template = `
+    <div class="col-lg-2 mb-2 text-center">
+    <div class="card border-0 rounded-0">
+      <div class="card-image">
+        <img src="${url_image}" alt="blog-img" class="img-fluid">
+      </div>
+    </div>
+    <div class="card-body text-capitalize">
+      <div class="card-meta fs-6">
+        <span class="meta-date">${book_author}</span>
+        <span class="meta-category">/ <a href="blog.html">${year_publication}</a></span>
+      </div>
+      <h4 class="card-title">
+        <a href="buy.html">${book_title}</a>
+      </h4>
+    </div>
+    </div>
+    `
+    postsElement.innerHTML += template;
+
+  })
 
 }
 
-cargarLibrary();
+loadBooks();
 
